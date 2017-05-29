a4164af1bd8ebcf3e377b1d4a47752f4b8f8f750
François Leurent <fleurent@ivsweb.com>
2017-05-29 22:50:10 +0200

    Standalone version

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 "use strict";
 
+const cp    = require('child_process');
 const path  = require('path');
-const passthru = require('nyks/child_process/passthru');
+
+const once = function(fn) {
+  var f = function () {
+    if (f.called) return f.value
+    f.called = true
+    return f.value = fn.apply(this, arguments)
+  }
+  f.called = false
+  return f
+}
+
+
+const passthru = function(cmd /*, options, chain*/){
+  var args = Array.from(arguments),
+      chain    = once(args.pop()),
+      cmd      = args.shift(),
+      options  = args.shift() || {};
+
+  if(Array.isArray(options))
+    options = { args : options};
+
+  options.stdio = ['inherit', 'inherit', 'inherit'];
+
+  var ps   = cp.spawn(cmd, options.args || [], options);
+
+  ps.on('error', chain);
+
+  ps.on('close', function(exit) {
+    var err = null;
+    if(exit !== 0)
+      err = "Bad exit code " + exit;
+    return chain(err, exit);
+  });
+}
+
 
 const phantomjs_bin = path.join(__dirname, 'bin', 'phantomjs');
 
